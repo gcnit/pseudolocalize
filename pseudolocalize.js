107a2b8db4123df28ab3242103732141a3be6dc9
Gaurav Chandak <gaurav01101993@gmail.com>
2019-02-02 01:40:48 +0530

    created pseudo localization page

---
+++ pseudolocalize.js
@@ -0,0 +1,67 @@
+const vowels = 'aeiou';
+
+const map = {
+    a: 'ȧ',
+    A: 'Ȧ',
+    b: 'ƀ',
+    B: 'Ɓ',
+    c: 'ƈ',
+    C: 'Ƈ',
+    d: 'ḓ',
+    D: 'Ḓ',
+    e: 'ḗ',
+    E: 'Ḗ',
+    f: 'ƒ',
+    F: 'Ƒ',
+    g: 'ɠ',
+    G: 'Ɠ',
+    h: 'ħ',
+    H: 'Ħ',
+    i: 'ī',
+    I: 'Ī',
+    j: 'ĵ',
+    J: 'Ĵ',
+    k: 'ķ',
+    K: 'Ķ',
+    l: 'ŀ',
+    L: 'Ŀ',
+    m: 'ḿ',
+    M: 'Ḿ',
+    n: 'ƞ',
+    N: 'Ƞ',
+    o: 'ǿ',
+    O: 'Ǿ',
+    p: 'ƥ',
+    P: 'Ƥ',
+    q: 'ɋ',
+    Q: 'Ɋ',
+    r: 'ř',
+    R: 'Ř',
+    s: 'ş',
+    S: 'Ş',
+    t: 'ŧ',
+    T: 'Ŧ',
+    v: 'ṽ',
+    V: 'Ṽ',
+    u: 'ŭ',
+    U: 'Ŭ',
+    w: 'ẇ',
+    W: 'Ẇ',
+    x: 'ẋ',
+    X: 'Ẋ',
+    y: 'ẏ',
+    Y: 'Ẏ',
+    z: 'ẑ',
+    Z: 'Ẑ'
+};
+
+const pseudoLocalize = (text) => {
+    let localizedText = '';
+    const characters = text.split('');
+    characters.forEach(character => {
+        const isVowel = vowels.indexOf(character.toLowerCase()) > -1;
+        const char = map[character] || character;
+        localizedText = localizedText.concat(char.repeat(isVowel + 1));
+    });
+    return localizedText;
+};
